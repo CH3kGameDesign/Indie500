13f000b074f267dee2c9ee06a0310e8d70409e14
Nathan Crowe <n.crowe.gamedesign@gmail.com>
2026-07-16 17:05:00 +1000

    wip: Bus Model & Movement

--- I500/Assets/Scripts/PlayerController.cs
+++ I500/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    #region References
+    [Header("References")]
     public Rigidbody RB_playerPhysics;
     public Transform T_playerModel;
     public Transform T_cameraHook;
-
+    #endregion
     #region Input
     private inputClass Input = new inputClass();
     class inputClass
@@ -20,16 +22,30 @@ public class PlayerController : MonoBehaviour
     private Vector3 q_rotLook = new Vector3();
     private Vector3 q_rotCameraHook = new Vector3();
     private Vector3 q_rotPlayerModel = new Vector3();
+    private bool b_grounded = false;
+    private bool b_canJump = true;
+    #endregion
+    #region Movement
+    [Header("Movement")]
     public float F_moveSpeed = 10;
+    public float F_airMultiplier = 2f;
+    public float F_groundDrag = 10;
+    public float F_jumpForce = 10;
+    public float F_jumpTimer = 0.1f;
+    public float F_playerHeight = 1f;
+    public LayerMask LM_ground = new LayerMask();
+    #endregion
+    #region Camera
+    [Header("Camera")]
     public float F_lookSpeed = 10;
-    public Vector2 V2_xLookBounds = new Vector2(-80,80);
+    public Vector2 V2_xLookBounds = new Vector2(-80, 80);
     #endregion
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SetCursorLock(true);
     }
-    
+
     void SetCursorLock(bool _locked)
     {
         Cursor.visible = !_locked;
@@ -47,13 +63,50 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        JumpHandler();
         PhysicsMovement();
+    }
+    private void JumpHandler()
+    {
+        GroundCheck();
+        if(b_canJump && b_grounded)
+        {
+            if (Input.B_jump)
+            {
+                b_canJump = false;
+                RB_playerPhysics.linearVelocity = new Vector3(RB_playerPhysics.linearVelocity.x, 0f, RB_playerPhysics.linearVelocity.z);
+                RB_playerPhysics.AddForce(transform.up * F_jumpForce, ForceMode.Impulse);
+                Invoke(nameof(ResetJump), F_jumpTimer);
+            }
+        }
+    }
+    private void ResetJump() { b_canJump = true; }
+    private void GroundCheck()
+    {
+        b_grounded = Physics.Raycast(RB_playerPhysics.position, Vector3.down, F_playerHeight * 0.5f + 0.1f, LM_ground);
+        if (b_grounded)
+            RB_playerPhysics.linearDamping = F_groundDrag;
+        else
+            RB_playerPhysics.linearDamping = 0;
     }
 
     void PhysicsMovement()
     {
-        v3_rotMove = Quaternion.Euler(q_rotPlayerModel) * new Vector3(Input.V2_Move.x, 0, Input.V2_Move.y) * F_moveSpeed * Time.fixedDeltaTime;
+        v3_rotMove = Quaternion.Euler(q_rotPlayerModel) * new Vector3(Input.V2_Move.x, 0, Input.V2_Move.y) * 10 * F_moveSpeed * Time.fixedDeltaTime;
+        if (!b_grounded) v3_rotMove *= F_airMultiplier;
         RB_playerPhysics.AddForce(v3_rotMove, ForceMode.Impulse);
+        SpeedControl();
+    }
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(RB_playerPhysics.linearVelocity.x, 0f, RB_playerPhysics.linearVelocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > F_moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * F_moveSpeed;
+            RB_playerPhysics.linearVelocity = new Vector3(limitedVel.x, RB_playerPhysics.linearVelocity.y, limitedVel.z);
+        }
     }
     void ModelMovement()
     {
@@ -70,8 +123,8 @@ public class PlayerController : MonoBehaviour
         T_playerModel.localEulerAngles = q_rotPlayerModel;
         T_cameraHook.localEulerAngles = q_rotCameraHook;
     }
-    
-    public void Input_Move (InputAction.CallbackContext cxt) { Input.V2_Move = cxt.ReadValue<Vector2>(); }
-    public void Input_Look (InputAction.CallbackContext cxt) { Input.V2_Look = cxt.ReadValue<Vector2>(); }
+
+    public void Input_Move(InputAction.CallbackContext cxt) { Input.V2_Move = cxt.ReadValue<Vector2>(); }
+    public void Input_Look(InputAction.CallbackContext cxt) { Input.V2_Look = cxt.ReadValue<Vector2>(); }
     public void Input_Jump(InputAction.CallbackContext cxt) { Input.B_jump = cxt.performed; }
 }
